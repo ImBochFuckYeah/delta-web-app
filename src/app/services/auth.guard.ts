923943a5a84bc80e08d10459bd9891da13a06406
CulajayP <alvizures1988luis@gmail.com>
2025-09-17 10:03:47 -0600

    COMENTARIO.?

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -14,6 +14,7 @@ export class AuthGuard implements CanActivate {
       return true;
     } else {
       this.router.navigate(['/forbidden']);
+      //this.router.navigate(['/login']);
       return false;
     }
   }
